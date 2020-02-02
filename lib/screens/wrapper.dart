7f88402ff24457895e389780d5439fb6dcb75efc
moshyfawn <oleg.butorov.98@gmail.com>
2020-02-02 18:35:50 +0300

    init basic app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/screens/home/home.dart' show Home;
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home / auth
+    return Home();
+  }
+}
